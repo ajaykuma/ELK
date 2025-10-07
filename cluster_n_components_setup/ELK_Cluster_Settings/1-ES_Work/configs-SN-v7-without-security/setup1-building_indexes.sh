148d0b173c484664ec385456fdb4af4fdfc9ae71
Ajay <ajayuma24@gmail.com>
2025-10-07 06:04:31 +0200

    updated

--- cluster_n_components_setup/ELK_Cluster_Settings/1-ES_Work/configs-SN-v7-without-security/setup1-building_indexes.sh
+++ cluster_n_components_setup/ELK_Cluster_Settings/1-ES_Work/configs-SN-v7-without-security/setup1-building_indexes.sh
@@ -13,7 +13,7 @@ curl -s http://localhost:9200/_cat/indices?v
 
 curl -XPUT "http://localhost:9200/ecommerce" -H "Content-Type: application/json" -d '{"settings":{"number_of_shards":3,"number_of_replicas":2,"auto_expand_replicas":false}}'
 curl -XPUT "http://localhost:9200/recipes" -H "Content-Type: application/json" -d '{"settings":{"number_of_shards":3,"number_of_replicas":2,"auto_expand_replicas":false}}'
-curl -XPUT "http://localhost:9200/customers" -H "Content-Type: application/json" -d '{"settings":{"number_of_shards":3,"number_of_replicas":2,"auto_expand_replicas":false}}'
+curl -XPUT "http://localhost:9200/customers2" -H "Content-Type: application/json" -d '{"settings":{"number_of_shards":3,"number_of_replicas":2,"auto_expand_replicas":false}}'
 
 #The request body must be newline-delimited JSON (NDJSON) 
 #— each action line (index) must be followed by its document line
@@ -25,7 +25,7 @@ echo 'loading data in recipes'
 curl -XPOST 'http://localhost:9200/recipes/_bulk' -H "Content-Type: application/json" --data-binary @datasets/recipes2.json
 
 echo 'loading data in customers'
-curl -XPOST 'http://localhost:9200/customers/_bulk' -H "Content-Type: application/json" --data-binary @datasets/customers2_compact.json
+curl -XPOST 'http://localhost:9200/customers2/_bulk' -H "Content-Type: application/json" --data-binary @datasets/customers2_compact.json
 
 echo '----------------------'
 echo 'Counts'
@@ -46,7 +46,7 @@ curl -XGET "http://localhost:9200/recipes/_count" \
        }
      }'
 
-curl -XGET "http://localhost:9200/customers/_count" \
+curl -XGET "http://localhost:9200/customers2/_count" \
      -H "Content-Type: application/json" \
      -d '{
        "query": {
