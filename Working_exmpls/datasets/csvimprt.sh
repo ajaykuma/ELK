b65d0851baae3621a790626119db9ff5c9e70c84
Ajay <ajaykuma24@gmail.com>
2025-10-07 01:46:42 +0000

    updated

---
+++ Working_exmpls/datasets/csvimprt.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+sudo apt install -y npm
+sudo npm install -g csvtojson
+csvtojson Bank_full.csv > bankdata.json
+#Elasticsearch bulk API needs action line + document line
+jq -c '.[] | {"index":{"_index":"bankdata"}} , .' bankdata.json > bankdata_bulk.json
+curl -s -H "Content-Type: application/json" -XPOST 'http://localhost:9200/_bulk' --data-binary @bankdata_bulk.json
+curl -XGET "http://localhost:9200/bankdata/_count" \
+     -H "Content-Type: application/json" \
+     -d '{
+       "query": {
+         "match_all": {}
+       }
+     }'
