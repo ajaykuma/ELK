ae76fd26f0a17c07808676db18caafd68ad7c2ba
Ajay <ajayuma24@gmail.com>
2025-10-07 05:26:56 +0200

    updated

---
+++ cluster_n_components_setup/ELK_Cluster_Settings/1-ES_Work/configs-SN-v7-without-security/setup1-building_indexes.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -e
+cd /home/hdu
+
+start_time=$(date +%s)
+
+echo 'listing nodes'
+curl -s http://localhost:9200/_cat/nodes?v
+
+echo 'listing indexes'
+curl -s http://localhost:9200/_cat/indices?v
+
+curl -XPUT "http://localhost:9200/ecommerce" -H "Content-Type: application/json" -d '{"settings":{"number_of_shards":3,"number_of_replicas":2,"auto_expand_replicas":false}}'
+curl -XPUT "http://localhost:9200/recipes" -H "Content-Type: application/json" -d '{"settings":{"number_of_shards":3,"number_of_replicas":2,"auto_expand_replicas":false}}'
+curl -XPUT "http://localhost:9200/customers" -H "Content-Type: application/json" -d '{"settings":{"number_of_shards":3,"number_of_replicas":2,"auto_expand_replicas":false}}'
+
+#The request body must be newline-delimited JSON (NDJSON) 
+#— each action line (index) must be followed by its document line
+
+echo 'loading data in ecommerce'
+curl -XPOST "http://localhost:9200/ecommerce/_bulk" -H "Content-Type: application/json" --data-binary @datasets/bulk_data.json
+
+echo 'loading data in recipes'
+curl -XPOST 'http://localhost:9200/recipes/_bulk' -H "Content-Type: application/json" --data-binary @datasets/recipes2.json
+
+echo 'loading data in customers'
+curl -s -H "Content-Type: application/json" -XPOST 'http://localhost:9200/_bulk' --data-binary @customers2_compact.json
+
+GET ecommerce/_count
+GET recipes/_count
+GET customers/_count
